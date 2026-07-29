83a5e2661e5eb6bbdbe2a7faa8ccf1a28b737fef
Dryet <lolgg088@gmail.com>
2026-07-29 23:39:56 +0300

    feat: aded base models

---
+++ PlaylistLinkParser/Models/PlaylistInfo.cs
@@ -0,0 +1,8 @@
+﻿namespace PlaylistLinkParser.Models;
+
+public class PlaylistInfo
+{
+    public string Name { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
+    public string AvatarUrl { get; set; } = string.Empty;
+}
---
+++ PlaylistLinkParser/Models/TrackInfo.cs
@@ -0,0 +1,9 @@
+﻿namespace PlaylistLinkParser.Models;
+
+public class TrackInfo
+{
+    public string Title { get; set; } = string.Empty;
+    public string Artist { get; set; } = string.Empty;
+    public string Album { get; set; } = string.Empty;
+    public string Duration { get; set; } = string.Empty;
+}
